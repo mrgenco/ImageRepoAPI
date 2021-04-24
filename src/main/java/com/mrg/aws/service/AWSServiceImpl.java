aae444fdd1cb9240869608b989e10bed80d28836
mrgenco <mrgenco@gmail.com>
2021-04-24 14:17:52 +0300

    dynamodb search funcionality

--- src/main/java/com/mrg/aws/service/AWSServiceImpl.java
+++ src/main/java/com/mrg/aws/service/AWSServiceImpl.java
@@ -1,5 +1,6 @@
 package com.mrg.aws.service;
 
+import com.mrg.aws.model.Image;
 import com.mrg.aws.util.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +12,9 @@ import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.core.ResponseBytes;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.core.sync.ResponseTransformer;
+import software.amazon.awssdk.enhanced.dynamodb.*;
+import software.amazon.awssdk.enhanced.dynamodb.model.QueryConditional;
+import software.amazon.awssdk.enhanced.dynamodb.model.ScanEnhancedRequest;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import software.amazon.awssdk.services.dynamodb.model.*;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -21,8 +25,7 @@ import software.amazon.awssdk.services.s3.model.S3Object;
 import software.amazon.awssdk.utils.IoUtils;
 
 import java.io.File;
-import java.util.HashMap;
-import java.util.UUID;
+import java.util.*;
 
 @Service
 public class AWSServiceImpl implements AWSService {
@@ -36,16 +39,63 @@ public class AWSServiceImpl implements AWSService {
 
     private S3Client s3Client;
     private DynamoDbClient dynamoDbClient;
+    private DynamoDbEnhancedClient enhancedClient;
 
     @Autowired
-    public AWSServiceImpl(S3Client s3Client, DynamoDbClient dynamoDbClient){
+    public AWSServiceImpl(S3Client s3Client, DynamoDbClient dynamoDbClient) {
         this.s3Client = s3Client;
         this.dynamoDbClient = dynamoDbClient;
+        this.enhancedClient = DynamoDbEnhancedClient.builder()
+                .dynamoDbClient(dynamoDbClient)
+                .build();
     }
 
 
     @Override
-    public byte[] download(UUID uniqueFileId) {
+    public List<Image> findAll() {
+        List<Image> imageList = new ArrayList<>();
+        try {
+            // Create a DynamoDbTable object
+            DynamoDbTable<Image> imageTable = enhancedClient.table(tableName,TableSchema.fromBean(Image.class));
+            imageTable.scan().items().forEach(imageList::add);
+            return imageList;
+
+        } catch (DynamoDbException e) {
+            LOGGER.error("Error= {} while searching DynamoDB.", e.getMessage());
+            throw e;
+        }
+    }
+
+    @Override
+    public List<Image> findBySearchTerm(String searchTerm) {
+        List<Image> imageList = new ArrayList<>();
+        try {
+            DynamoDbTable<Image> imageTable = enhancedClient.table(tableName,TableSchema.fromBean(Image.class));
+            AttributeValue attr = AttributeValue.builder()
+                    .s(searchTerm)
+                    .build();
+            Map<String, AttributeValue> myMap = new HashMap<>();
+            myMap.put(":tag", attr);
+            Expression expression = Expression.builder()
+                    .expressionValues(myMap)
+                    .expression("contains(Tags, :tag)")
+                    .build();
+            ScanEnhancedRequest enhancedRequest = ScanEnhancedRequest.builder()
+                    .filterExpression(expression)
+                    .limit(15)
+                    .build();
+
+            imageTable.scan(enhancedRequest).items().forEach(imageList::add);
+            return imageList;
+
+        } catch (DynamoDbException e) {
+            LOGGER.error("Error= {} while searching DynamoDB.", e.getMessage());
+            throw e;
+        }
+    }
+
+    @Override
+    public byte[] downloadImage(UUID uniqueFileId) {
         try {
             GetObjectRequest getObjectRequest = GetObjectRequest.builder()
                     .bucket(bucketName)
@@ -56,13 +106,13 @@ public class AWSServiceImpl implements AWSService {
             return result.asByteArray();
 
         } catch (Exception e) {
-            throw new IllegalStateException("Failed to download file from s3 +["+uniqueFileId+"]", e);
+            throw new IllegalStateException("Failed to download file from s3 +[" + uniqueFileId + "]", e);
         }
     }
 
     @Override
-    public void uploadFile(final MultipartFile multipartFile, final String description, final String tags) throws Exception {
-       if(FileUtils.isFileNotEmpty(multipartFile)) {
+    public void uploadImage(final MultipartFile multipartFile, final String description, final String tags) throws Exception {
+        if (FileUtils.isFileNotEmpty(multipartFile)) {
             LOGGER.info("File upload in progress.");
             // Primary key for S3 & DynamoDB records
             final String uniqueFileId = UUID.randomUUID().toString();
@@ -75,24 +125,38 @@ public class AWSServiceImpl implements AWSService {
     }
 
 
-
     private void addFileToDynamoDBTable(String uniqueId, MultipartFile multipartFile, String description, String tags) {
         try {
+            LOGGER.info("Adding DynamoDB record with name= " + uniqueId);
+            DynamoDbTable<Image> imageTable = enhancedClient.table(tableName,
+                    TableSchema.fromBean(Image.class));
+            Image image = new Image();
+            image.setImageId(uniqueId);
+            image.setFileName(multipartFile.getOriginalFilename());
+            image.setFileDesc(description);
+            image.setFileType(multipartFile.getContentType());
+            image.setFileSize(String.valueOf(multipartFile.getSize()));
+            image.setTags(tags);
+            // Put the customer data into a DynamoDB table
+            imageTable.putItem(image);
+            LOGGER.info("File upload DynamoDB is completed.");
+
+            /*
             LOGGER.info("Adding DynamoDB record with name= " + uniqueId);
             HashMap<String, AttributeValue> itemValues = new HashMap<String, AttributeValue>();
             itemValues.put("ImageId", AttributeValue.builder().s(uniqueId).build());
             itemValues.put("FileName", AttributeValue.builder().s(multipartFile.getOriginalFilename()).build());
             itemValues.put("FileType", AttributeValue.builder().s(multipartFile.getContentType()).build());
             itemValues.put("FileSize", AttributeValue.builder().s(String.valueOf(multipartFile.getSize())).build());
-            itemValues.put("Description", AttributeValue.builder().s(description).build());
+            itemValues.put("FileDesc", AttributeValue.builder().s(description).build());
             itemValues.put("Tags", AttributeValue.builder().s(tags).build());
             PutItemRequest request = PutItemRequest.builder()
                     .tableName(tableName)
                     .item(itemValues)
                     .build();
             PutItemResponse response = dynamoDbClient.putItem(request);
-            LOGGER.info("File upload DynamoDB is completed. Response : " + response.sdkHttpResponse());
-        } catch (Exception ex) {
+            */
+        } catch (DynamoDbException ex) {
             LOGGER.error("Error= {} while adding new file to DynamoDB.", ex.getMessage());
             throw ex;
         }
@@ -110,24 +174,24 @@ public class AWSServiceImpl implements AWSService {
             s3Client.putObject(objectRequest, RequestBody.fromFile(file));
             LOGGER.info("File upload S3 is completed.");
 
-        }catch (Exception ex) {
+        } catch (Exception ex) {
             LOGGER.error("Error= {} while uploading file to S3.", ex.getMessage());
             // Rollback operation for logical data consistency
             rollBackFromDynamoDBTable(uniqueFileId);
             throw ex;
-        }finally {
+        } finally {
             FileUtils.deleteIfExist(file);
         }
     }
 
 
     /*
-    * TR : S3 operasyonu başarısız olursa mantıksal veri bütünlüğünü korumak
-    *      adına ilgili DynamoDB kaydı silinmeli/geri alınmalı.
-    * -------------------------------------------------------------------------------
-    * EN : If S3 operation fails ensures the remaining DynamoDB record is rolled back
-    *      for logical data consistency.
-    * */
+     * TR : S3 operasyonu başarısız olursa mantıksal veri bütünlüğünü korumak
+     *      adına ilgili DynamoDB kaydı silinmeli/geri alınmalı.
+     * -------------------------------------------------------------------------------
+     * EN : If S3 operation fails ensures the remaining DynamoDB record is rolled back
+     *      for logical data consistency.
+     * */
     private void rollBackFromDynamoDBTable(String uniqueFileId) {
         try {
 
